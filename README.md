f5a75e4594c73ba2e8f2b618e5223380e59ac4dd
Rachel Willmer <rachel@willmer.org>
2012-09-21 00:29:22 +0100

    Sent to pypi

--- README.md
+++ README.md
@@ -22,5 +22,12 @@ HOW TO USE
 REQUIREMENTS
 ============
 
-See requirements.txt.
+The main one is the 'behave' module, of course, which provides the BDD toolset for Python.
+
+Also used are:
+- django >= 1.4 (needed for the LiveServerTestCase)
+- selenium
+- splinter
+
+See requirements.txt for details.
 
--- setup.py
+++ setup.py
@@ -6,7 +6,13 @@ setup(
 	description = 'Django Test Runner for the Behave BDD module',
 	author = 'Rachel Willmer',
 	author_email = 'rachel@willmer.org',
+        url = 'https://github.com/rwillmer/django-behave',
 	classifiers = [
+                'Programming Language :: Python :: 2.7',
 		'Framework :: Django',
+                'Development Status :: 3 - Alpha',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: Apache Software License',
+                'Topic :: Software Development :: Testing',
 	],
 )
