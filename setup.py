f968c5de44f21cdc7fcda436bfd75a301a5737fc
Rachel Willmer <rachel@willmer.org>
2012-09-22 15:17:47 +0100

    Update MANIFEST to include steps_library.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
 	name = 'django-behave',
 	packages = ['django-behave'],
-	version = '0.0.3',
+	version = '0.0.4',
 	description = 'Django Test Runner for the Behave BDD module',
 	author = 'Rachel Willmer',
 	author_email = 'rachel@willmer.org',
