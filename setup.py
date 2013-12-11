e0400adb9da9129394b8a3c8728a4f001482ead8
Adam Stein <adam@csh.rit.edu>
2013-12-11 14:54:35 -0500

    Added the ability to pass in options.  Because of this, all of Behave's command line options can now be passed in via Django's manage.py test command as well as a new django-behave option to choose which browser to use

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='django-behave',
     packages=['django_behave'],
-    version='0.0.12',
+    version='0.0.13',
     description='Django Test Runner for the Behave BDD module',
     author='Rachel Willmer',
     author_email='rachel@willmer.org',
