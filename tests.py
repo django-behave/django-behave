a349974e35bdc061b3129615ec8b73591e56a252
dgreisen-cfpb <david.greisen@cfpb.gov>
2014-01-29 14:02:49 -0500

    run_test() method works; behave args properly passed in

--- tests.py
+++ tests.py
@@ -5,13 +5,15 @@ class BehaveTest(unittest.TestCase):
     def run_test(self, app='example_app', settings='example_proj.settings', *args, **kwargs):
         """
         test the given app with the given args and kwargs passed to manage.py. kwargs are converted from
-        {'a': 'b'} to --a=b
+        {'a': 'b'} to --a b
 
         returns a tuple: (stdout, stderr)
         """
+        args = list(args)
         kwargs['settings'] = settings
-        kwargs = ['--{}={}'.format(k, v) for k, v in kwargs.items()]
-        p = subprocess.Popen(['./manage.py', 'test', app] + list(args) + kwargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        for k, v in kwargs.items():
+            args += ['--{}'.format(k), v]
+        p = subprocess.Popen(['./manage.py', 'test', app] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return p.communicate()
 
     def test_runner_with_default_args_expect_bdd_tests_run(self):
@@ -25,7 +27,7 @@ class BehaveTest(unittest.TestCase):
         self.assertNotIn('scenario passed', actual[0])
 
     def test_runner_with_old_tag_specified_expect_only_old_bdd_test_run(self):
-        actual = self.run_test('--behave_tags @old')
+        actual = self.run_test(behave_tags='@old')
 
         self.assertIn('1 scenario passed, 0 failed, 1 skipped', actual[0])
 
