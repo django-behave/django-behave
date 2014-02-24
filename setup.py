3b94c2cac9ff1f4bac46a30810ab7af1b69d3024
Rachel Willmer <rachel@hobthross.com>
2014-02-24 14:21:30 +0000

    Bumped version to v0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='django-behave',
     packages=['django_behave'],
-    version='0.1.0',
+    version='0.1.1',
     description='Django Test Runner for the Behave BDD module',
     author='Rachel Willmer',
     author_email='rachel@willmer.org',
